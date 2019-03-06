2c4192529d0b5e1e9a87a953bfbc70b1f83dce64
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-06 20:43:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,15 +7,16 @@ class CashRegister
     @items = []
   end
   
-  def add_items(title, price, quantity = 1)
-    if quantity > 1
-      i = 0
-      while i < quantity
-      @items << title
-      i += 1
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
     else
       @items << title
-  end
+    end
     @total += price*quantity
     @last_transaction_amount = @total
     @total
