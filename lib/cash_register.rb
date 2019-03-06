0a30b3f5fac608d9b7a49ee663652fd64bd0807a
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-06 20:41:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,5 +8,11 @@ class CashRegister
   end
   
   def add_items(title, price, quantity = 1)
-    
+    if quantity > 1
+      i = 0
+      while i < quantity
+      @items << title
+      i += 1
+    end
+  end
 end
